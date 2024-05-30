9761a608c6f2c64671652d38245ec1698bf257eb
2216000292 <65697537+2216000292@users.noreply.github.com>
2024-05-29 21:00:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -663,6 +663,19 @@ const divObservers = {};
  * @property {string} color - The color input for the bar chart from the form.
  * @property {boolean} projection - The projection status, converted to boolean.
  */
+const datasetString = `[
+  {"x": 0, "y": 28, "c":0}, {"x": 0, "y": 20, "c":1},
+  {"x": 1, "y": 43, "c":0}, {"x": 1, "y": 35, "c":1},
+  {"x": 2, "y": 81, "c":0}, {"x": 2, "y": 10, "c":1},
+  {"x": 3, "y": 19, "c":0}, {"x": 3, "y": 15, "c":1},
+  {"x": 4, "y": 52, "c":0}, {"x": 4, "y": 48, "c":1},
+  {"x": 5, "y": 24, "c":0}, {"x": 5, "y": 28, "c":1},
+  {"x": 6, "y": 87, "c":0}, {"x": 6, "y": 66, "c":1},
+  {"x": 7, "y": 17, "c":0}, {"x": 7, "y": 27, "c":1},
+  {"x": 8, "y": 68, "c":0}, {"x": 8, "y": 16, "c":1},
+  {"x": 9, "y": 49, "c":0}, {"x": 9, "y": 25, "c":1}
+]`;
+document.getElementById('ember30').value = datasetString;
 const form = document.querySelector('#my-form');
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // prevent the default form submission behavior
